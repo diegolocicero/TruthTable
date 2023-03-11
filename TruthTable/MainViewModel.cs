c10d50e8d4198c9cfcc87c92748d1e0ce833d009
catri <catrini.alessio2@gmail.com>
2023-03-11 10:14:17 +0100

    Non lo so

--- TruthTable/MainViewModel.cs
+++ TruthTable/MainViewModel.cs
@@ -1,9 +1,17 @@
-﻿using GalaSoft.MvvmLight.Command;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows.Input;
+using System;
+using System.Linq;
+using GalaSoft.MvvmLight.Command;
+namespace TruthTable
+{
 
-namespace TruthTable {
-    public class MainViewModel {
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         #region
         public ICommand SendInputCommand { get; set; } //ICommand per settare l'input, si attiva con il button
         public string Input { get; set; } //Prop in binding con la textbox 
@@ -13,31 +21,60 @@ namespace TruthTable {
         public ObservableCollection<Carattere> Lettere { get; set; }
         public ObservableCollection<Carattere> Segni { get; set; }
 
-        public MainViewModel() {
+        public ObservableCollection<Carattere> LettereSegni
+        {
+            get
+            {
+                var result = new ObservableCollection<Carattere>(Lettere.Concat(Segni));
+                result.Insert(0, new Carattere(' '));
+                return result;
+            }
+        }
+
+        public int NColonne => LettereSegni.Count;
+        public int NRighe => (int)Math.Pow(2, Lettere.Count) + 1;
+
+        public MainViewModel()
+        {
             SendInputCommand = new RelayCommand(SendInput);
             Lettere = new ObservableCollection<Carattere>();
             Segni = new ObservableCollection<Carattere>();
         }
+
         public void SendInput() //Assegna a lettere e segni le lettere e i segni, e grazie al cazzo (scarta i numeri)
         {
             Lettere.Clear();
             Segni.Clear();
-            foreach (char ch in Input) {
-                if (!char.IsDigit(ch) && ch != ' ') {
+            foreach (char ch in Input)
+            {
+                if (!char.IsDigit(ch) && ch != ' ')
+                {
                     if (ch == '+' || ch == '*')
                         Segni.Add(new Carattere(ch));
                     else
                         Lettere.Add(new Carattere(ch));
                 }
             }
+
+            OnPropertyChanged(nameof(LettereSegni));
+            OnPropertyChanged(nameof(NColonne));
+            OnPropertyChanged(nameof(NRighe));
         }
 
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
-    public class Carattere {
+
+    public class Carattere
+    {
         public char Carac { get; set; }
-        public Carattere(char carattere) {
+        public Carattere(char carattere)
+        {
             Carac = carattere;
         }
     }
-}
 
+
+}
